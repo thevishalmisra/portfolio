fbfa9220384fce56f7005d8599841368b7f58bdf
Vishal Raman <154840534+thevishalmisra@users.noreply.github.com>
2025-06-14 02:35:54 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyB4IBPDJDwaKI79nHWK6sbApct9eGLpQB4",
+  apiKey: "AIzaSyB4IBPDJDwaKI79nHWK6sbApct9eGLpQB4",
   authDomain: "portfolio-a48e0.firebaseapp.com",
   projectId: "portfolio-a48e0",
   storageBucket: "portfolio-a48e0.firebasestorage.app",
   messagingSenderId: "907335177479",
   appId: "1:907335177479:web:d05a632a3d091bd56d21a2"
+
 };
 
 // Initialize Firebase
